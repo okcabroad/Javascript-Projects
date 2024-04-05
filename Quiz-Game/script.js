b7e5dd9ea9c6bc543d25f5ffafc43c62c83053b1
Oli <okcabroad@gmail.com>
2024-04-05 13:27:32 +0900

    inital commit

---
+++ Quiz-Game/script.js
@@ -0,0 +1,34 @@
+const quizQuestions = [
+    {   questionNumber: 1,
+        question: "What country has the highest life expectancy?",
+        correctAnswer: ['Hong Kong'],
+        wrongAnswer: ['Iran', 'South Korea', 'Germany']
+    },
+    {   questionNumber: 2,
+        question: "Where would you be if you were standing on the Spanish Steps? ",
+        correctAnswer: ['Rome'],
+        wrongAnswer: ['Milan', 'Barcelona', 'Lyon']
+    },
+    {   questionNumber: 3,
+        question: "What is the most common surname in the United States?",
+        correctAnswer: ['Smith'],
+        wrongAnswer: ['Clarke', 'Williams', 'Johnson']
+    },
+    {   questionNumber: 4,
+        question: "How many elements are in the periodic table?",
+        correctAnswer: ['118'],
+        wrongAnswer: ['120', '112', '110']
+    },
+    {   questionNumber: 5,
+        question: "Which planet has the most moons?",
+        correctAnswer: ['Saturn'],
+        wrongAnswer: ['Jupiter', 'Neptune', 'Uranus']
+    },
+    {   questionNumber: 6,
+        question: "How many Stars on the Chinese Flag?",
+        correctAnswer: ['5'],
+        wrongAnswer: ['7', '6', '9']
+    },
+]
+
+console.log(quizQuestions[5].wrongAnswer[1]);
