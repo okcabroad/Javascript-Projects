e55fc0f7008d004bbda314373d7c89ba6f703d91
Oli <okcabroad@gmail.com>
2024-04-05 14:00:06 +0900

    adding event listeners and functions to the start

--- Quiz-Game/script.js
+++ Quiz-Game/script.js
@@ -1,3 +1,5 @@
+//Quiz questions array of objects
+// console.log(quizQuestions[5].wrongAnswer[1]);
 const quizQuestions = [
     {   questionNumber: 1,
         question: "What country has the highest life expectancy?",
@@ -31,4 +33,47 @@ const quizQuestions = [
     },
 ]
 
-console.log(quizQuestions[5].wrongAnswer[1]);
+
+//DOM variables
+const questionContainer = document.querySelector('.questionContainer');
+const question = document.querySelector('.question');
+const optionsContainer = document.querySelector('.optionsContainer');
+const options = document.querySelector('.options')
+const answersContainer = document.querySelector('.answersContainer');
+
+const startButton = document.querySelector('.startButton'); 
+const button1 = document.querySelector('.button1');
+const button2 = document.querySelector('.button2');
+const button3 = document.querySelector('.button3');
+const button4 = document.querySelector('.button4');
+
+let questionCompleted =[];
+
+//Event Listeners
+startButton.addEventListener('click', startGame);
+
+
+
+//Functions
+
+function generateRandomNumber(){
+    randomNumber = Math.floor(Math.random() * quizQuestions.length); //generates a number between 0 - quiz array length
+    return randomNumber;
+}
+
+
+
+function startGame(){
+    const thisTurnNumber = generateRandomNumber()
+    question.textContent = quizQuestions[thisTurnNumber].question;
+
+    options.remove();
+    optionsContainer.innerHTML = `
+        <p class="options">${quizQuestions[thisTurnNumber].correctAnswer}</p>
+        <p class="options">${quizQuestions[thisTurnNumber].wrongAnswer[0]}</p>
+        <p class="options">${quizQuestions[thisTurnNumber].wrongAnswer[1]}</p>
+        <p class="options">${quizQuestions[thisTurnNumber].wrongAnswer[2]}</p>
+    `
+}
+
+
