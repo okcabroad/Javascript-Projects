5a7aa6a64e3f55bc7c36a5ecd84485ca753441aa
Oli <okcabroad@gmail.com>
2024-04-06 20:20:24 +0900

    adding game calculateBMI function and styling

--- BMI/script.js
+++ BMI/script.js
@@ -1,1 +1,17 @@
-console.log('connnected');
+heightInput = document.querySelector('#height');
+weightInput = document.querySelector('#weight');
+bmiButton = document.querySelector('.bmiButton');
+bmiDisplay = document.querySelector('.bmiDisplay');
+
+bmiButton.addEventListener('click', calculateBMI);
+
+function calculateBMI(){
+
+    const height = parseInt(heightInput.value) / 100;
+    const weight = parseInt(weightInput.value);
+
+    const BMI = weight / (height * height);
+
+    bmiDisplay.textContent = `BMI: ${BMI.toFixed(1)}`;
+
+}
