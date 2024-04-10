0917feace94089f7d5f1091acec253ccd31e1300
Oli <okcabroad@gmail.com>
2024-04-10 21:20:55 +0900

    making calulator work with two number inputs

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,7 +1,11 @@
 const input = document.querySelector('.input');
 const buttons = document.querySelectorAll('.button');
+const numbers = [1,2,3,4,5,6,7,8,9];
 let calculation;
-
+let currentNumber = [];
+let previousNumber = [];
+let operator = [];
+let answer = null;
 
 buttons.forEach(button => {
     button.addEventListener('click', clickButton);
@@ -12,22 +16,82 @@ function clickButton(event){
     let clickedbutton = event.target.textContent;
     input.value += clickedbutton;
     clearDisplay(event);
-    calculate(event);
+    
+    if(/[0-9]/.test(clickedbutton)){
+        currentNumber += clickedbutton;
+    }
+    else if(/[+\-*\/]/.test(clickedbutton)){
+        previousNumber = currentNumber;
+        currentNumber = [];
+        operator += clickedbutton;
+    }
+    else if(clickedbutton == '='){
+        switch (operator){
+            case '+':
+                answer = Number(previousNumber) + Number(currentNumber);
+                console.log(answer);
+                currentNumber = answer;
+                previousNumber = [];
+                operator = [];
+                break;
+            case '-':
+                answer = Number(previousNumber) - Number(currentNumber);
+                console.log(answer);
+                currentNumber = answer;
+                previousNumber = [];
+                operator = [];
+                break;
+            case '*':
+                answer = Number(previousNumber) * Number(currentNumber);
+                console.log(answer);
+                currentNumber = answer;
+                previousNumber = [];
+                operator = [];
+                break;
+            case '/':
+                answer = Number(previousNumber) / Number(currentNumber);
+                console.log(answer);
+                currentNumber = answer;
+                previousNumber = [];
+                operator = [];
+                break;
+            default:
+                answer = 'Error';
+                console.log(answer);
+                currentNumber = [];
+                previousNumber = [];
+                operator = [];
+                break;
+        }   
+        input.value = answer;
+
+    }
+
+    
 
 }
 
 function clearDisplay(button){
     if(button.target.textContent == 'C'){
         input.value = '';
+        answer = null;
+        currentNumber = [];
+        previousNumber = [];
+        operator = [];
+
+
     }
 }
 
 
-function calculate(button){
-    if(button.target.textContent == '='){
-        for(let i = 0; i < input.value.length; i++){
-            calculation += parseInt(input.value[i]);
-            console.log(calculation);
-        }
-    }
-}
+// function calculate(button){
+
+//     if(button.target.textContent == '='){
+//         for(let i = 0; i < input.value.length; i++){
+//             if(/[0-9]/.test(input.value[i])){
+//                 num1 += input.value[i];
+//             }
+            
+//         }
+//     }
+// }
