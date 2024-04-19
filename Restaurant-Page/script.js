4423c0ba95c4b9cfbbf1bb80943a9312ce022b0e
okc <okcabroad@gmail.com>
2024-04-20 00:11:59 +0900

    last commit

--- Restaurant-Page/script.js
+++ Restaurant-Page/script.js
@@ -1,1 +1,7 @@
-console.log('connected');
+const button = document.querySelector('button');
+
+button.addEventListener('click', openWiki);
+
+function openWiki(){
+    window.open('https://en.wikipedia.org/wiki/Indian_cuisine', '_blank');
+}
